d3c8fdca10a6b0aa411b1f88ac65eaa823a49ed3
tczarnia <tristanczk@yahoo.fr>
2023-10-22 17:26:38 +0200

    scraping finished without recursion

--- Module00_Arachnida/ex01_Spider/spider.py
+++ Module00_Arachnida/ex01_Spider/spider.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 import argparse
 import requests
 import sys
+import os
+from urllib.parse import urlsplit
+
+extensions = [".jpeg", ".jpg", ".png", ".gif", ".bmp"]
+count = 0
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(prog='Spider', description='A program that downloads image from a given URL.')
@@ -15,19 +20,63 @@ def parse_arguments():
 		parser.error('-l option cannot be used without -r')
 	elif args.r and not args.l:
 		args.l = 5
+	elif args.r and args.l < 1:
+		parser.error('Depth level cannot be less than 1')
 	return args
 
-def main():
-	extensions = ["jpeg", "jpg", "png", "gif", "bmp"]
-	args = parse_arguments()
-	try:
-		request = requests.get(args.url)
-	except:
-		print('Provided URL is not valid.', file=sys.stderr)
-		return
-	print(request.text)
-	
-	
+def make_output_dir(path):
+	if not os.path.exists(path):
+		try:
+			os.mkdir(path)
+		except FileNotFoundError:
+			print(f'Provided path for output directory is not valid: {path}', file=sys.stderr)
+			sys.exit(1)
 
+def scrape_images(url, output_dir, depth_level, max_depth_level):
+	global count
+	if depth_level > max_depth_level:
+		return
+	try:
+		request = requests.get(url)
+	except:
+		print(f'Provided URL is not valid: {url}', file=sys.stderr)
+		return
+	soup = BeautifulSoup(request.text, 'html.parser')
+	split_url = urlsplit(url)
+
+	images = soup.find_all('img')
+	for image in images:
+		image_url = image.attrs.get('src')
+		if not image_url:
+			print('Could not find image URL', file=sys.stderr)
+			continue
+		_, file_extension = os.path.splitext(image_url)
+		if file_extension not in extensions:
+			continue
+		if not (image_url.startswith('http')):
+			if image_url.startswith('//'):
+				image_url = split_url.scheme + ':' + image_url
+			elif image_url.startswith('/'):
+				image_url = split_url.scheme + '://' + split_url.netloc + image_url
+			else:
+				image_url = split_url.scheme + '://' + split_url.netloc + '/' + image_url
+		with open(output_dir + 'image' + str(count) + file_extension, 'wb') as f:
+			try:
+				r = requests.get(image_url)
+				f.write(r.content)
+				count += 1
+				print(f"{count} images downloaded", end="\r")
+			except:
+				continue
+
+def main():
+	args = parse_arguments()
+	make_output_dir(args.p)
+	if args.r:
+		scrape_images(args.url, args.p, 1, args.l)
+	else:
+		scrape_images(args.url, args.p, 1, 1)
+	print(f'{count} images downloaded')
+	
 if __name__ == '__main__':
 	main()
