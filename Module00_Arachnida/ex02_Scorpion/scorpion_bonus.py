92fa1f933a18f2c050800f780bfe128ba3f34d75
tczarnia <tristanczk@yahoo.fr>
2023-10-27 19:01:52 +0200

    beginning graphical interface

--- Module00_Arachnida/ex02_Scorpion/scorpion_bonus.py
+++ Module00_Arachnida/ex02_Scorpion/scorpion_bonus.py
@@ -1,7 +1,8 @@
 import sys
 import os
-import pyexiv2
 import PySimpleGUI as sg
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 extensions = [".jpeg", ".jpg", ".png", ".gif", ".bmp"]
 
@@ -12,65 +13,61 @@ fields = {
 	"YResolution": "Vertical Resolution",
 	"Software": "Software",
 	"DateTime": "Last Modified",
-	"ExposureTime": "Exposure Time",
-	"FNumber": "F-Number",
-	"ISOSpeedRatings": "ISO Speed Ratings",
-	"DatetimeOriginal": "Date Taken",
-	"ShutterSpeedValue": "Shutter Speed",
-	"ApertureValue": "Aperture",
-	"BrightnessValue": "Brightness",
-	"ExposureBiasValue": "Exposure Bias",
-	"Flash": "Flash",
-	"FocalLength": "Focal Length",
-	"PixelXDimension": "Width",
-	"PixelYDimension": "Height",
-	"FocalPlaneXResolution": "Focal Plane X-Resolution",
-	"FocalPlaneYResolution": "Focal Plane Y-Resolution",
-	"BodySerialNumber": "Body Serial Number",
+	"ResolutionUnit": "Resolution Unit",
 }
 
-def handle_file_metadata(file):
-	print(f"-----Image: {file}-----")
+def handle_file_metadata(file, window):
 	_, file_extension = os.path.splitext(file)
 	if file_extension not in extensions:
-		print(f'File {file} extension is not valid. Accepted files are: .jpeg, .jpg, .png, .gif, .bmp.\n', file=sys.stderr)
+		window["status"].update("File extension is not valid. Accepted files are: .jpeg, .jpg, .png, .gif, .bmp.", visible=True)
+		for key in fields:
+			window[key].update("")
 		return
 	try:
-		metadata = pyexiv2.ImageMetadata(file)
-		metadata.read()
+		image = Image.open(file)
 	except Exception as e:
-		print(f'Error opening the file: {e}\n', file=sys.stderr)
+		window["status"].update("Error opening the file", visible=True)
+		for key in fields:
+			window[key].update("")
 		return
-	for key in metadata.exif_keys:
-		print(f"{key.split('.')[-1]:25}: {metadata[key].value}")
-	print()
+	exifdata = image.getexif()
+	window["status"].update("", visible=False)
+	for tag_id in exifdata:
+		tag = TAGS.get(tag_id, tag_id)
+		data = exifdata.get(tag_id)
+		if isinstance(data, bytes):
+			try:
+				data = data.decode()
+			except:
+				continue
+		if tag in fields:
+			window[tag].update(data)
 
 def main():
 	if (len(sys.argv) < 2):
 		print('Usage: python3 File1 [File2 ...]', file=sys.stderr)
 		return		
-	
+	sg.theme("DarkGrey5")
 	sg_elements = [
 		[
 			sg.Text("Select an image: ", size=(20, 1)),
 			sg.Combo(sys.argv[1:], size=(50, 1), key="image", enable_events=True),
 		],
-		[sg.Button("OK")]
+		[sg.Text("", key="status", text_color="red", visible=False, justification="center", size=(70, 1))]
 	]
+	for key, value in fields.items():
+		sg_elements.append([sg.Text(f"{value}: ", size=(20, 1)), sg.Text("", size=(50, 1), key=key)])
 	window = sg.Window("Scorpion", sg_elements)
-	# for arg in sys.argv[1:]:
-	# 	handle_file_metadata(arg)
 	while True:
 		event, values = window.read()
 
 		if event == sg.WINDOW_CLOSED:
 			break
-		elif event == "OK":
+		elif event == "image":
 			selected_file = values["image"]
-			sg.popup(f"You selected: {selected_file}")
+			handle_file_metadata(selected_file, window)
 
 	window.close()
-
 
 if __name__ == '__main__':
 	main()
