1834f49f02f46da0d729d1de624d51decff51f75
tczarnia <tristanczk@yahoo.fr>
2023-11-25 15:06:31 +0100

    finished password generation, need to check result

--- Module01_ft_otp/ft_otp.py
+++ Module01_ft_otp/ft_otp.py
@@ -2,6 +2,8 @@
 import sys
 from check_args import parse_arguments, verify_args
 from cryptography.fernet import Fernet
+import hmac
+from datetime import datetime
 
 
 encryption_key_file = "encryption_key.key"
@@ -29,9 +31,31 @@ def generate_key_file(key):
             f.write(cipher.encrypt(key))
     except Exception as e:
         print(f"./ft_otp: error: {e}", file=sys.stderr)
+        return
+    print("Key was successfully saved in ft_otp.key.")
+
+
+# based on https://www.rfc-editor.org/rfc/rfc4226#section-5.3
+# and https://medium.com/zeals-tech-blog/implementing-your-own\
+# -time-based-otp-generator-3b971a31330b
+def hotp(key, count):
+    '''generate hotp password'''
+    count_bytes = count.to_bytes(8, byteorder="big")
+    hmac_result = hmac.new(key, count_bytes, "sha1").digest()
+    offset = hmac_result[19] & 15
+    bin_code = ((hmac_result[offset] & 127) << 24 |
+                (hmac_result[offset + 1] & 255) << 16 |
+                (hmac_result[offset + 2] & 255) << 8 |
+                (hmac_result[offset + 3] & 255)
+                )
+    return bin_code % 10**6
 
 
 def generate_password(key):
+    '''generate hotp password based on time passed since unix epoch'''
+    today = datetime.today()
+    count = int(today.timestamp() // 30)
+    return hotp(key, count)
 
 
 def main():
@@ -42,7 +66,7 @@ def main():
     if args.g:
         generate_key_file(key)
     else:
-        generate_password(key)
+        print(generate_password(key))
 
 
 if __name__ == "__main__":
